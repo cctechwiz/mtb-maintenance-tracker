dea59f9daf9781bb6fb184cc6b587d4fee164e6b
Stephen Brown <stephen.jr36@gmail.com>
2024-09-09 15:59:44 -0600

    added routes for react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { 
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements
+} from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
+import Dashboard from './pages/Dashboard.jsx'
+import Builds from './pages/Builds.jsx'
+import Parts from './pages/Parts.jsx'
+import Rides from './pages/Rides.jsx'
+import Maintenance from './pages/Maintenance.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<Dashboard />} />
+      <Route path='/builds' element={<Builds />} />
+      <Route path='/parts' element={<Parts />} />
+      <Route path='/rides' element={<Rides />} />
+      <Route path='/maintenance' element={<Maintenance />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
