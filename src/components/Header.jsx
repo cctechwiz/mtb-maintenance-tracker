d4bf7f2782cbbd44d4d88d423ca95c2f3811bd7a
Stephen Brown <stephen.jr36@gmail.com>
2024-09-09 15:03:23 -0600

    created Header boilerplate

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <span>Logo</span>
+      <ul>
+        <li>Builds</li>
+        <li>Parts</li>
+        <li>Rides</li>
+        <li>Maintenance</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
