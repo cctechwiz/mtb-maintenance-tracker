c76c3f859601554dcc914b226b6688582867d160
Stephen Brown <stephen.jr36@gmail.com>
2024-09-09 16:26:18 -0600

    added BuildItem component

--- src/pages/Builds.jsx
+++ src/pages/Builds.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import BuildItem from '../components/BuildItem.jsx'
 
 const Builds = () => {
   return (
-    <h1>Builds</h1>
+    <>
+      <h1>Builds</h1>
+      <BuildItem />
+    </>
   )
 }
 
