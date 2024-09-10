d2c06b93c20327cc26992090f6a2f86882e8fa35
Stephen Brown <stephen.jr36@gmail.com>
2024-09-10 11:08:41 -0600

    added react route for /settings

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import Parts from './pages/Parts.jsx'
 import Rides from './pages/Rides.jsx'
 import Maintenance from './pages/Maintenance.jsx'
 import Auth from './pages/Auth.jsx'
+import Settings from './pages/Settings.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
       <Route path='/parts' element={<Parts />} />
       <Route path='/rides' element={<Rides />} />
       <Route path='/maintenance' element={<Maintenance />} />
+      <Route path='/settings' element={<Settings />} />
       <Route path='/auth' element={<Auth />} />
     </Route>
   )
