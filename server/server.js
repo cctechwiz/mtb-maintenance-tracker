0540ccd334379fbf2d7411a63afc0681c8032bb0
Stephen Brown <stephen.jr36@gmail.com>
2024-09-11 13:41:06 -0600

    added express server boilerplate

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from 'morgan';
+import session from 'express-session';
+import ViteExpress from 'vite-express';
+
+import { handlerFunctions } from './controller.js';
+
+const app = express();
+const port = '8080';
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  session({
+    secret: 'hello',
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.get('/api/session-check', handlerFunctions.sessionCheck);
+app.post('/api/login', handlerFunctions.login);
+app.get('/api/logout', handlerFunctions.logout);
+app.post('/api/register', handlerFunctions.register);
