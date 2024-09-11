d3e5a78e8725a87782696cafbe689a99c45e2607
Stephen Brown <stephen.jr36@gmail.com>
2024-09-11 15:47:27 -0600

    added handlerFunctions.login logic

--- server/controller.js
+++ server/controller.js
@@ -11,10 +11,41 @@ export const handlerFunctions = {
   },
 
   login: async (req, res) => {
+    const { email, password } = req.body;
 
+    // Query db for user based on email from request body
+    const user = await User.findOne({
+      where: {
+        email: email
+      }
+    });
+
+    // if 'user' is falsy (no user was found in db)
+    if (!user) {
+      return res.send({
+        message: 'no user found',
+        success: false
+      });
+    };
+
+    // if hash of password from req.body does not match password hash from db
+    if (!bcryptjs.compareSync(password, user.password)) {
+      return res.send({
+        message: 'password is incorrect',
+        success: false
+      });
+    };
+
+    // If user exists and password is correct, save user.id to session
+    req.session.userId = user.id
+
+    return res.send({
+      message: 'user logged in',
+      success: true
+    });
   },
 
   logout: async (req, res) => {
-    
+
   }
 }
