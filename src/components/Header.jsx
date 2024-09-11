613254230c3f19338f8e06ecfde1134cb7826b12
Stephen Brown <stephen.jr36@gmail.com>
2024-09-11 16:33:37 -0600

    made navlinks not visible if user is not logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,37 @@
 import { NavLink } from 'react-router-dom'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const userId = useSelector((state) => state.userId);
+
+  console.log(`USERID:`, userId)
+  
   return (
     <div>
       <NavLink to='/'>Logo</NavLink>
-      <ul>
-        <li>
-          <NavLink to='/builds'>Builds</NavLink>
-        </li>
-        <li>
-          <NavLink to='/parts'>Parts</NavLink>
-        </li>
-        <li>
-          <NavLink to='/rides'>Rides</NavLink>
-        </li>
-        <li>
-          <NavLink to='/maintenance'>Maintenance</NavLink>
-        </li>
-        <li>
-          <NavLink to='/settings'>User Settings</NavLink>
-        </li>
-        <li>
-          <NavLink to='/auth'>Login</NavLink>
-        </li>
-      </ul>
+      {userId &&
+        <ul>
+          <li>
+            <NavLink to='/builds'>Builds</NavLink>
+          </li>
+          <li>
+            <NavLink to='/parts'>Parts</NavLink>
+          </li>
+          <li>
+            <NavLink to='/rides'>Rides</NavLink>
+          </li>
+          <li>
+            <NavLink to='/maintenance'>Maintenance</NavLink>
+          </li>
+          <li>
+            <NavLink to='/settings'>User Settings</NavLink>
+          </li>
+          <li>
+            <NavLink to='/auth'>Login</NavLink>
+          </li>
+        </ul>
+      }
     </div>
   )
 }
