e6cf13b09d8bc344694d1310d1fe1780fc8bbe9d
Stephen Brown <stephen.jr36@gmail.com>
2024-09-12 17:03:54 -0600

    in process of adding parts to parts object to create part categories and part types

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import { db, User } from "./model.js";
 import bcryptjs from 'bcryptjs';
 
 await db.sync({ force: true });
 
-let users = ['user1@test.com']
+const users = ['user1@test.com']
 
 const hashedPassword = bcryptjs.hashSync('test', bcryptjs.genSaltSync(10));
 
@@ -12,6 +13,94 @@ for (const user of users) {
     email: user.toLowerCase(),
     password: hashedPassword
   })
+}
+
+/*
+Part Hierarchy
+
+Frame:
+  - frame
+  - bottom bracket
+  - headset
+  - derailleur hanger
+
+Fork:
+  - fork
+
+Suspension:
+  - shock
+  - linkage bearings (or busings)
+
+Cockpit:
+  - stem
+  - handlebars
+  - grips
+  - saddle
+  - seatpost
+  - dropper post lever
+  - pedals (is this in the right category?)
+
+Brakes:
+(do these need to be separate? how do you differentiate between a service on one vs the other?)
+  - front brakes:
+    -- levers
+    -- lines (should this be included with levers?)
+    -- calipers
+    -- rotors
+  - rear brakes:
+    -- levers
+    -- lines (should this be included with levers?)
+    -- calipers
+    -- rotors
+
+Groupset:
+  - shifter(s)
+  - if mechanical:
+    -- housing and cable(s)
+  - if wireless:
+    -- battery
+  - derailleur(s)
+  - crank
+  - chainring
+  - chain
+
+Wheels:
+  - front wheel
+    -- rim
+    -- hub
+    -- spokes
+    -- tire
+    if tubes:
+      --- tube
+    if tubeless:
+      --- valve stem
+      --- sealant
+    -- valve core
+    -- front axel
+  - rear wheel
+    -- rim
+    -- hub
+    -- spokes
+    if tubes:
+      --- tube
+    if tubeless:
+      --- valve stem
+      --- sealant
+    -- valve core
+    -- freehub
+    -- rear axel
+*/
+
+const partTypes = ['frame', 'fork', 'suspension', 'cockpit', 'brakes', 'groupset', 'wheels']
+
+const parts = { 
+  frame: ['frame', 'bottomBracket', 'headset', 'derailleurHanger'],
+  fork: ['fork'],
+  suspension: ['shock', 'linkageBearings'],
+  cockpit: ['stem', 'handlebars', 'grips', 'saddle', 'seatpost', 'dropperLever', 'pedals'],
+  brakes: ['frontLever', 'rearLever', 'frontBrakeLine', 'rearBrakeLine', 'frontCaliper', 'rearCaliper', 'frontRotor', 'rearRotor'],
+  groupset: [],
+  wheels: []
 }
 
 await db.close();
