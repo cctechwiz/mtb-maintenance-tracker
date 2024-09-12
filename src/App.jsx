0358bf7240063fb73db7ae69b22a0a5d5568ae1c
Stephen Brown <stephen.jr36@gmail.com>
2024-09-12 11:39:30 -0600

    added sessionCheck fn to save userId from session to Redux store, or navigate to /auth route if user is not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header.jsx'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const userId = useSelector((state) => state.userId);
-
-  useEffect(() => {
-    if (!userId) {
+  
+  // Upon refresh, check if user session exists
+  const sessionCheck = async () => {
+    // Call server to see if userId is saved in session
+    const res = await axios.get('/api/session-check');
+    
+    if (res.data.success) {
+      // If userId was in session, dispatch userId from response to Redux store
+      dispatch({
+        type: 'USER_AUTH',
+        payload: res.data.userId
+      })
+    } else {
+      // If no userId was in session, navigate to login page
       navigate('/auth')
-    }
-  }, [])
+    };
+  };
+  
+  useEffect(() => {
+    sessionCheck();
+  }, []);
 
   return (
     <>
