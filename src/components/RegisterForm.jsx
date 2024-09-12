4eb66cd6cc86e37a25c7e4b8bdb7cc3c0e35f91c
Stephen Brown <stephen.jr36@gmail.com>
2024-09-12 14:34:09 -0600

    added handleRegister fn to add new user to db

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,36 +1,63 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = ({ setShowRegister }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const bodyObj = {
+      email,
+      password,
+      name,
+    };
+
+    // Make API call to register new user to db
+    const res = await axios.post('/api/register', bodyObj);
+
+    if (res.data.success) {
+      dispatch({
+        type: 'USER_AUTH',
+        payload: res.data.userId
+      });
+
+      navigate('/')
+    } else {
+      // TODO: change to toastify
+      setErrorMessage(res.data.message);
+    };
+  }
 
   return (
     <>
       <div>
         <h1>Register</h1>
       </div>
+
+      <div>
+        {/* TODO: change to toastify */}
+        {errorMessage &&
+          <p>{errorMessage}</p>
+        }
+      </div>
       
-      <form>
+      <form onSubmit={handleRegister}>
         <div>
-          {/* <label htmlFor="email">Email:</label> */}
           <input
             value={email}
             type="email"
             id='email'
             placeholder='email'
             onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div>
-          {/* <label htmlFor='password'>Password:</label> */}
-          <input
-            value={password}
-            type="password"
-            id='password'
-            placeholder='password'
-            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -42,6 +69,20 @@ const RegisterForm = ({ setShowRegister }) => {
             placeholder='name'
             onChange={(e) => setName(e.target.value)}
           />
+        </div>
+
+        <div>
+          <input
+            value={password}
+            type="password"
+            id='password'
+            placeholder='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input type="submit" />
         </div>
       </form>
 
