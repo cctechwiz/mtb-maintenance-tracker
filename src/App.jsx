c9fe871c9183ddb70955464836be66a95a1c48e9
Stephen Brown <stephen.jr36@gmail.com>
2024-09-12 10:32:40 -0600

    added useEffect to route user to /auth if no user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header.jsx'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  const userId = useSelector((state) => state.userId);
+
+  useEffect(() => {
+    if (!userId) {
+      navigate('/auth')
+    }
+  }, [])
 
   return (
     <>
