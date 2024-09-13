7127f49adc960f2e306e6b6b0f8263fcb456bf8e
Stephen Brown <stephen.jr36@gmail.com>
2024-09-13 15:26:12 -0600

    added new build button and logic to display NewBuildForm component

--- src/pages/Builds.jsx
+++ src/pages/Builds.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BuildItem from '../components/BuildItem.jsx'
+import NewBuildForm from '../components/NewBuildForm.jsx';
+import { useSelector } from 'react-redux';
 
 const Builds = () => {
+  const [ displayForm, setDisplayForm ] = useState(false);
+
+  const builds = useSelector((state) => state.builds);
+
+  console.log(`builds:`, builds)
+
   return (
     <>
       <h1>Builds</h1>
+      {!displayForm && <button onClick={() => setDisplayForm(true)}>Add New Build</button>}
+      {displayForm && <NewBuildForm />}
       <BuildItem />
     </>
   )
