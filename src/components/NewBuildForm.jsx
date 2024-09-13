e2e5dbc15e684011a2944df8fa20c4f920679776
Stephen Brown <stephen.jr36@gmail.com>
2024-09-13 15:25:06 -0600

    added logic to make axios POST request with bodyObj. Still missing success and failure logic

--- src/components/NewBuildForm.jsx
+++ src/components/NewBuildForm.jsx
@@ -1,17 +1,35 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 const NewBuildForm = () => {
   const [ name, setName ] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userId = useSelector((state) => state.userId)
 
   const handleNewBuild = async (e) => {
     e.preventDefault();
 
     const bodyObj = {
-      name
-    }
+      buildName: name,
+      userId: userId
+    };
 
     const res = await axios.post('/api/new-build', bodyObj);
+
+    if (res.data.success) {
+      dispatch({
+        type: 'UPDATE_BUILDS',
+        payload: res.data.builds
+      })
+    }
+
+    // console.log(res.data.message)
+    // console.log(`BUILDS:`, res.data.builds)
+
+
   }
 
   return (
