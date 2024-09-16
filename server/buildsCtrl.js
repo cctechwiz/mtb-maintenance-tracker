54ad42f3706bc2baad5059c135c39f8a510f8dd0
Stephen Brown <stephen.jr36@gmail.com>
2024-09-16 15:18:15 -0600

    relocated getBuildsData fn to buldsCtrl.js

--- server/buildsCtrl.js
+++ server/buildsCtrl.js
@@ -1,6 +1,48 @@
-import { User, Build } from "../db/model.js";
+import { User, Build, PartCategory, PartType, Part } from "../db/model.js";
 
 export const buildFuncs = {
+  getBuildsData: async (req, res) => {
+    // QUESTION: should this be userId from Redux or the session?
+    const userId = req.session.userId;
+
+    const builds = await Build.findAll({
+      where: {
+        userId: userId,
+      }
+    });
+
+    const partsData = await Promise.all(builds.map((build) => {
+      return PartCategory.findAll({
+        include: {
+          model: PartType,
+          include: {
+            model: Part,
+            include: {
+              model: Build,
+              where: { id: build.id }
+            }
+          }
+        }
+      })
+    }))
+
+    console.log(`partsData:`, partsData[0])
+
+    if (!builds) {
+      return res.send({
+        message: 'Failed to get builds data',
+        success: false
+      });
+    } else {
+      return res.send({
+        message: 'Got builds data successfully',
+        success: true,
+        builds: builds,
+        parts: partsData[0]
+      });
+    };
+  },
+
   newBuild: async (req, res) => {
     const { buildName, userId } = req.body;
 
