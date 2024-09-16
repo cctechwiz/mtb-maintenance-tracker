03dbdc5e9f5ee167d88c0f4a44f8d24fc2126b3b
Stephen Brown <stephen.jr36@gmail.com>
2024-09-16 10:36:14 -0600

    removed case for new build (no longer using Redux to store build data)

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,5 @@
 const initialState = {
   userId: null,
-  builds: null
 };
 
 const reducer = (state = initialState, action) => {
@@ -15,12 +14,6 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         userId: null
-      }
-    
-    case 'UPDATE_BUILDS':
-      return {
-        ...state,
-        builds: action.payload
       }
 
     default:
