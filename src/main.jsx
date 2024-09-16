5540c2b7b5564c58ea9d3d6b44eddd5da353cd73
Stephen Brown <stephen.jr36@gmail.com>
2024-09-16 12:21:50 -0600

    added loader to /builds route

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,67 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { 
-  Route, 
-  RouterProvider, 
-  createBrowserRouter, 
-  createRoutesFromElements
-} from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import axios from "axios";
+import { Provider } from "react-redux";
 
-import App from './App.jsx'
-import './index.css'
-import Dashboard from './pages/Dashboard.jsx'
-import Builds from './pages/Builds.jsx'
-import Parts from './pages/Parts.jsx'
-import Rides from './pages/Rides.jsx'
-import Maintenance from './pages/Maintenance.jsx'
-import Auth from './pages/Auth.jsx'
-import Settings from './pages/Settings.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import App from "./App.jsx";
+import "./index.css";
+import Dashboard from "./pages/Dashboard.jsx";
+import Builds from "./pages/Builds.jsx";
+import Parts from "./pages/Parts.jsx";
+import Rides from "./pages/Rides.jsx";
+import Maintenance from "./pages/Maintenance.jsx";
+import Auth from "./pages/Auth.jsx";
+import Settings from "./pages/Settings.jsx";
+import store from "./redux/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
+    <Route path="/" element={<App />}>
       <Route index element={<Dashboard />} />
-      <Route path='/builds' element={<Builds />} />
-      <Route path='/parts' element={<Parts />} />
-      <Route path='/rides' element={<Rides />} />
-      <Route path='/maintenance' element={<Maintenance />} />
-      <Route path='/settings' element={<Settings />} />
-      <Route path='/auth' element={<Auth />} />
+      <Route
+        path="/builds"
+        element={<Builds />}
+        loader={
+          async () => {
+            const res = await axios.get('/api/builds');
+            return { builds: res.data.builds }
+          }
+        }
+      />
+      <Route
+        path="/parts"
+        element={<Parts />}
+      />
+      <Route
+        path="/rides"
+        element={<Rides />}
+      />
+      <Route
+        path="/maintenance"
+        element={<Maintenance />}
+      />
+      <Route
+        path="/settings"
+        element={<Settings />}
+      />
+      <Route
+        path="/auth"
+        element={<Auth />}
+      />
     </Route>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
