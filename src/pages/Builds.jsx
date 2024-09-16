0ccfd0d6e4e08c0af72e9499727d552f7fc9bb3a
Stephen Brown <stephen.jr36@gmail.com>
2024-09-16 13:40:08 -0600

    BuildItem components are created for each build returned in React Router loader function

--- src/pages/Builds.jsx
+++ src/pages/Builds.jsx
@@ -1,21 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BuildItem from '../components/BuildItem.jsx'
 import NewBuildForm from '../components/NewBuildForm.jsx';
 import { useSelector } from 'react-redux';
+import { useLoaderData } from 'react-router-dom';
 
 const Builds = () => {
   const [ displayForm, setDisplayForm ] = useState(false);
+  const [ buildsData, setBuildsData ] = useState([])
 
-  const builds = useSelector((state) => state.builds);
+  const { builds } = useLoaderData();
 
-  console.log(`builds:`, builds)
+  // QUESTION: Is a useEffect necessary, and why does the useEffect work here?
+  useEffect(() => {
+    setBuildsData(builds);
+  }, []);
+  
+  // console.log(`buildsData:`, buildsData)
+  
+  // QUESTION: 
+  const buildItems = buildsData.map((build) => {
+    return (
+      <BuildItem key={build.id} name={build.name} />
+    )
+  })
 
   return (
     <>
-      <h1>Builds</h1>
-      {!displayForm && <button onClick={() => setDisplayForm(true)}>Add New Build</button>}
-      {displayForm && <NewBuildForm />}
-      <BuildItem />
+      <div>
+        <h1>Builds</h1>
+        {!displayForm && <button onClick={() => setDisplayForm(true)}>Add New Build</button>}
+        {displayForm && <NewBuildForm />}
+      </div>
+        
+      <div>
+        { buildItems }
+      </div>
     </>
   )
 }
