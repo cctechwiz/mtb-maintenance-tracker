fcb7702f80a4abca77a1d5bebab4d9229d14bd64
Stephen Brown <stephen.jr36@gmail.com>
2024-09-17 16:45:28 -0600

    added title to new build form. Fixed prop name

--- src/components/NewBuildForm.jsx
+++ src/components/NewBuildForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
-const NewBuildForm = ({ setBuildsData, setDisplayForm }) => {
+const NewBuildForm = ({ setBuilds, setDisplayForm }) => {
   const [ name, setName ] = useState('');
   const [ newPartsSelected, setNewPartsSelected ] = useState(true);
 
@@ -31,31 +31,34 @@ const NewBuildForm = ({ setBuildsData, setDisplayForm }) => {
   }
 
   return (
-    <form onSubmit={handleNewBuild}>
-      <div>
-        <input
-          value={name}
-          type="text" 
-          placeholder='Build name'
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
+    <>
+      <h3>Create New Build</h3>
+      <form onSubmit={handleNewBuild}>
+        <div>
+          <input
+            value={name}
+            type="text" 
+            placeholder='Build name'
+            onChange={(e) => setName(e.target.value)}
+            />
+        </div>
 
-      <div>
-        <label htmlFor="create-new-parts">Create new parts for this build?</label>
-        <input
-          value={newPartsSelected}
-          type="checkbox"
-          checked={newPartsSelected}
-          id="create-new-parts"
-          onChange={handleNewPartsChange}
-        />
-      </div>
+        <div>
+          <label htmlFor="create-new-parts">Create new parts for this build?</label>
+          <input
+            value={newPartsSelected}
+            type="checkbox"
+            checked={newPartsSelected}
+            id="create-new-parts"
+            onChange={handleNewPartsChange}
+            />
+        </div>
 
-      <div>
-        <input type="submit" />
-      </div>
-    </form>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </>
   )
 }
 
