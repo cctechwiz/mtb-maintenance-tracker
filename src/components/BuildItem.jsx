3c652f678d2685fbd7d1140119791ead45303e17
Stephen Brown <stephen.jr36@gmail.com>
2024-09-17 16:24:14 -0600

    now generates CategoryItem components and passes down category name and parts data

--- src/components/BuildItem.jsx
+++ src/components/BuildItem.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { CategoryItem } from './CategoryItem'
-import PartItem from './PartItem'
+import React from 'react';
+import { CategoryItem } from './CategoryItem.jsx';
 
-const BuildItem = ({ name }) => {
+const BuildItem = ({ name, categoriesData }) => {
+  const categories = categoriesData.map((category) => {
+    return (
+      <CategoryItem 
+        key={category.categoryId}
+        name={category.categoryName}
+        partsData={category.parts}
+      />
+    );
+  });
+
   return (
     <div>
       <h2>{ name }</h2>
+      { categories }
     </div>
-  )
-}
+  );
+};
 
-export default BuildItem
+export default BuildItem;
