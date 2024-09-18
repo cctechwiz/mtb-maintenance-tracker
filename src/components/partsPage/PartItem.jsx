f09b0b6917aeaa9b380ce3baa96b912778f061fc
Stephen Brown <stephen.jr36@gmail.com>
2024-09-18 15:36:37 -0600

    parts page now displays parts within part types

---
+++ src/components/partsPage/PartItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PartItem = ({ name }) => {
+  return (
+    <li>{ name }</li>
+  )
+}
+
+export default PartItem
--- src/components/partsPage/PartTypeItem.jsx
+++ src/components/partsPage/PartTypeItem.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import PartItem from './PartItem';
 
-const PartTypeItem = ({ name }) => {
+const PartTypeItem = ({ name, data }) => {
+  console.log('partType data:', data);
+
+  const partItems = data.map((part) => {
+    return (
+      <PartItem
+        key={part.id}
+        name={part.name}
+      />
+    )
+  })
+
   return (
-    <h3>{ name }</h3>
+    <>
+      <h3>{ name }</h3>
+      <ul>
+        {partItems}
+      </ul>
+    </>
   )
 }
 
