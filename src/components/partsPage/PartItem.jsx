51a83f27fe75c50cda458bc3f35241d4d4b7ba8e
Stephen Brown <stephen.jr36@gmail.com>
2024-09-18 15:52:24 -0600

    build name will be 'none' if not installed on a build

--- src/components/partsPage/PartItem.jsx
+++ src/components/partsPage/PartItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const PartItem = ({ name, build }) => {
+  let buildName = build[0] ? build[0].name : 'none';
+
   return (
-    <li>Part name: { name } | installed on: {build[0].name}</li>
+    <li>Part name: { name } | installed on: {buildName}</li>
   )
 }
 
