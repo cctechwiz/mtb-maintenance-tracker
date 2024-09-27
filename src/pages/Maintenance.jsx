0f0ab059d6f0c3e1ae90567c428aea99a100450c
Stephen Brown <stephen.jr36@gmail.com>
2024-09-27 16:23:29 -0600

    service items are now displayed in order by date descending to keep parts from moving upon edit

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import NewServiceForm from '../components/maintPage/NewServiceForm.jsx';
-import ServiceItem from '../components/maintPage/ServiceItem.jsx';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import NewServiceForm from "../components/maintPage/NewServiceForm.jsx";
+import ServiceItem from "../components/maintPage/ServiceItem.jsx";
 
 const Maintenance = () => {
-  const [ services, setServices ] = useState(useLoaderData().maintData);
-  const [ displayForm, setDisplayForm ] = useState(false);
+  const [services, setServices] = useState(useLoaderData().maintData);
+  const [displayForm, setDisplayForm] = useState(false);
 
-  const serviceItems = services.map((service) => {
-    return (
-      <ServiceItem key={service.id} data={service} setServices={setServices} />
-    );
-  });
+  // console.log('services data', services)
+  const serviceItems = services
+    .sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+    .map((service) => {
+      console.log("service.date", service.date);
+      return (
+        <ServiceItem
+          key={service.id}
+          data={service}
+          setServices={setServices}
+        />
+      );
+    });
 
   return (
     <>
       <div>
         <h1>Maintenance</h1>
-        {!displayForm && <button onClick={() => setDisplayForm(true)}>Add Service</button>}
-        {displayForm && 
-          <NewServiceForm setDisplayForm={setDisplayForm} setServices={setServices} />
-        }
+        {!displayForm && (
+          <button onClick={() => setDisplayForm(true)}>Add Service</button>
+        )}
+        {displayForm && (
+          <NewServiceForm
+            setDisplayForm={setDisplayForm}
+            setServices={setServices}
+          />
+        )}
       </div>
 
       <div>
-        <ul>
-          { serviceItems }
-        </ul>
+        <ul>{serviceItems}</ul>
       </div>
     </>
   );
