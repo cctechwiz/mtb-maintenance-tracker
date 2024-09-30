a8982f1afa99fd24a1733f8f2b8520ee6f640525
Stephen Brown <stephen.jr36@gmail.com>
2024-09-30 12:17:54 -0600

    added basics responsive logic to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,98 @@
-import { NavLink, useNavigate } from 'react-router-dom'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios';
+import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+import { IoMdMenu } from "react-icons/io";
 
 const Header = () => {
   const userId = useSelector((state) => state.userId);
+
+  const [showMenu, setShowMenu] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
-    const res = await axios.get('/api/logout');
+    const res = await axios.get("/api/logout");
 
     if (res.data.success) {
       dispatch({
-        type: 'LOGOUT'
-      })
+        type: "LOGOUT",
+      });
 
-      navigate('/auth');
-    };
+      navigate("/auth");
+    }
   };
-  
-  return (
-    <div>
-      <NavLink to='/'>MTB Maintenance Tracker</NavLink>
-      {userId &&
-        <ul>
-          <li>
-            <NavLink to='/builds'>Builds</NavLink>
-          </li>
-          <li>
-            <NavLink to='/parts'>Parts</NavLink>
-          </li>
-          <li>
-            <NavLink to='/rides'>Rides</NavLink>
-          </li>
-          <li>
-            <NavLink to='/maintenance'>Maintenance</NavLink>
-          </li>
-          <li>
-            <NavLink to='/settings'>User Settings</NavLink>
-          </li>
-          <li>
-            <button onClick={handleLogout}>Logout</button>
-          </li>
-        </ul>
-      }
-    </div>
-  )
-}
 
-export default Header
+  return (
+    <>
+      {/* Mobile View */}
+      <nav className="flex lg:hidden justify-between h-[10vh] items-center px-8 bg-blue-dark text-white">
+        <div>
+          <NavLink to="/" >MTB Maintenance Tracker</NavLink>
+        </div>
+        <div>
+          {userId &&
+            <button>
+              <IoMdMenu onClick={() => setShowMenu(!showMenu)} />
+            </button>
+          }
+        </div>
+        {showMenu && (
+          <ul className="absolute left-0 top-[10vh] bg-blue-medium h-[90vh] px-5 pt-5">
+            <li>
+              <NavLink to="/builds">Builds</NavLink>
+            </li>
+            <li>
+              <NavLink to="/parts">Parts</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to='/rides'>Rides</NavLink>
+            </li> */}
+            <li>
+              <NavLink to="/maintenance">Maintenance</NavLink>
+            </li>
+            {/* <li>
+                <NavLink to='/settings'>User Settings</NavLink>
+            </li> */}
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        )}
+      </nav>
+
+      {/* Desktop View */}
+      <nav className="hidden lg:flex justify-between h-[10vh] items-center px-8 bg-blue-dark text-white">
+        <div>
+          <NavLink to="/">MTB Maintenance Tracker</NavLink>
+        </div>
+        {userId && (
+          <ul className="flex justify-between w-1/3">
+            <li>
+              <NavLink to="/builds">Builds</NavLink>
+            </li>
+            <li>
+              <NavLink to="/parts">Parts</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to='/rides'>Rides</NavLink>
+              </li> */}
+            <li>
+              <NavLink to="/maintenance">Maintenance</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to='/settings'>User Settings</NavLink>
+              </li> */}
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#00171F',
+        'blue-dark': '#003459',
+        'blue-medium': '#007EA7',
+        'blue-light': '#00A8E8'
+      }
+    },
   },
   plugins: [],
 }
