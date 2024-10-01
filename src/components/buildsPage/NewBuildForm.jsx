0ca088d2e081336e4976c0e98a04f6b0b82ac9ad
Stephen Brown <stephen.jr36@gmail.com>
2024-10-01 12:06:35 -0600

    added new build modal functinoality and styling

--- src/components/buildsPage/NewBuildForm.jsx
+++ src/components/buildsPage/NewBuildForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useSelector } from 'react-redux';
+import { HiMiniXMark } from "react-icons/hi2";
+
 
 const NewBuildForm = ({ setUserBuilds, setDisplayForm }) => {
   const [ name, setName ] = useState('');
@@ -38,33 +40,44 @@ const NewBuildForm = ({ setUserBuilds, setDisplayForm }) => {
 
   return (
     <>
-      <h3>Create New Build</h3>
-      <form onSubmit={handleNewBuild}>
-        <div>
-          <input
-            value={name}
-            type="text" 
-            placeholder='Build name'
-            onChange={(e) => setName(e.target.value)}
-            />
+      <div id="overlay" className='fixed top-0 left-0 right-0 bottom-0 bg-black/60 bg-transparent-70'></div>
+      <div id="modal" className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-3 z-1000 rounded-lg w-[95%] max-w-md md:max-w-lg lg:max-w-xl'>
+        <div className='flex justify-end items-start'>
+          <button type="button" onClick={() => setDisplayForm(false)}>
+            <HiMiniXMark className='h-6 w-6' />
+          </button>
         </div>
+        <h3 className='text-2xl mb-5 mx-3'>Create New Build</h3>
+        <form onSubmit={handleNewBuild} className='flex flex-col gap-4 mb-3 mx-3'>
+          <div className='flex flex-col'>
+            <label className='text-sm' htmlFor="build-name">Build Name<sup>*</sup>:</label>
+            <input
+              className='border-2 border-blue-medium rounded-md p-1'
+              value={name}
+              id='build-name'
+              type="text" 
+              placeholder='e.g. "Downhill" or "Trek Slash"'
+              onChange={(e) => setName(e.target.value)}
+              />
+          </div>
 
-        <div>
-          <label htmlFor="create-new-parts">Create new parts for this build?</label>
-          <input
-            value={newPartsSelected}
-            type="checkbox"
-            checked={newPartsSelected}
-            id="create-new-parts"
-            onChange={handleNewPartsChange}
-            />
-        </div>
+          <div className='flex items-center gap-3'>
+            <label htmlFor="create-new-parts">Create new parts for this build?</label>
+            <input
+              className='h-5 w-5'
+              value={newPartsSelected}
+              type="checkbox"
+              checked={newPartsSelected}
+              id="create-new-parts"
+              onChange={handleNewPartsChange}
+              />
+          </div>
 
-        <div>
-          <button type="button" onClick={() => setDisplayForm(false)}>Cancel</button>
-          <input type="submit" />
-        </div>
-      </form>
+          <div>
+            <input className='text-xl text-white bg-blue-light rounded-md px-4 py-1' type="submit" />
+          </div>
+        </form>
+      </div>
     </>
   )
 }
--- src/pages/Builds.jsx
+++ src/pages/Builds.jsx
@@ -8,8 +8,6 @@ const Builds = () => {
   const [ displayForm, setDisplayForm ] = useState(false);
   const [ userBuilds, setUserBuilds ] = useState(useLoaderData().userBuilds)
   const [ partCategories, setPartCategories ] = useState(useLoaderData().partCategories)
-
-  // console.log('userBuilds: ', userBuilds)
   
   // Create BuildItem components
   const buildItems = userBuilds.sort((a, b) => a.id - b.id).map((build, index) => {
@@ -21,21 +19,21 @@ const Builds = () => {
         setUserBuilds={setUserBuilds}
         // categoriesData={build.categories}
       />
-    )
-  })
+    );
+  });
 
   return (
     <div id='page-container' className='w-full max-w-xl pt-20'>
       <div id='title-container' className='flex p-5 gap-5 w-full justify-between'>
         <h1 className='text-4xl'>Builds</h1>
-        {!displayForm && 
+        {/* {!displayForm &&  */}
           <button
             className='text-white bg-blue-light rounded-md px-4'
             onClick={() => setDisplayForm(true)}
           >
             + New Build
           </button>
-        }
+        {/* } */}
       </div>
 
       <div>
