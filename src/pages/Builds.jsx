0965e2de6fd5bac83c5e58471c8091021ee927b4
Stephen Brown <stephen.jr36@gmail.com>
2024-10-01 13:15:35 -0600

    improved code formatting, removed comments

--- src/pages/Builds.jsx
+++ src/pages/Builds.jsx
@@ -6,8 +6,8 @@ import { useLoaderData } from 'react-router-dom';
 
 const Builds = () => {
   const [ displayForm, setDisplayForm ] = useState(false);
-  const [ userBuilds, setUserBuilds ] = useState(useLoaderData().userBuilds)
-  const [ partCategories, setPartCategories ] = useState(useLoaderData().partCategories)
+  const [ userBuilds, setUserBuilds ] = useState(useLoaderData().userBuilds);
+  const [ partCategories, setPartCategories ] = useState(useLoaderData().partCategories);
   
   // Create BuildItem components
   const buildItems = userBuilds.sort((a, b) => a.id - b.id).map((build, index) => {
@@ -26,25 +26,21 @@ const Builds = () => {
     <div id='page-container' className='w-full max-w-xl pt-20'>
       <div id='title-container' className='flex p-5 gap-5 w-full justify-between'>
         <h1 className='text-4xl'>Builds</h1>
-        {/* {!displayForm &&  */}
-          <button
-            className='text-white bg-blue-light rounded-md px-4'
-            onClick={() => setDisplayForm(true)}
-          >
-            + New Build
-          </button>
-        {/* } */}
+        <button
+          className='text-white bg-blue-light rounded-md px-4'
+          onClick={() => setDisplayForm(true)}
+        >
+          + New Build
+        </button>
       </div>
 
-      <div>
-        {displayForm && <NewBuildForm setUserBuilds={setUserBuilds} setDisplayForm={setDisplayForm} />}
-      </div>  
+      {displayForm && <NewBuildForm setUserBuilds={setUserBuilds} setDisplayForm={setDisplayForm} />}
         
       <div id='primary-container'>
         { buildItems }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Builds
+export default Builds;
